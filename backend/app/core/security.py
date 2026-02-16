7cb750ff18e7dc85054c1df50b47d6247f6f0ed6
hunde <hundetesfa6@gmail.com>
2026-02-16 22:10:34 +0300

    security check of authrization and authontication

---
+++ backend/app/core/security.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timedelta
+from typing import Optional
+import bcrypt
+from jose import JWTError, jwt
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from .database import get_db
+from .config import settings
+from ..models.user import User
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(plain_password.encode("utf-8"), hashed_password.encode("utf-8"))
+
+
+def get_password_hash(password: str) -> str:
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
+
+def verify_token(token: str, db: Session):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            return None
+    except JWTError:
+        return None
+    
+    user = db.query(User).filter(User.username == username).first()
+    return user
